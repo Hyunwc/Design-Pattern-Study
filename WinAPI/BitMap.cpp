fb5ab263c020146f80c068d50c95191d3d8820d1
jejekim86 <107088369+Hyunwc@users.noreply.github.com>
2024-05-07 20:50:18 +0900

    winapi

---
+++ WinAPI/BitMap.cpp
@@ -0,0 +1,32 @@
+#include "BitMap.h"
+
+
+
+BitMap::BitMap()
+{
+}
+
+void BitMap::Init(HDC hdc,char* FileName)
+{
+	MemDC = CreateCompatibleDC(hdc); //붓을 만드는.
+	//독립 리소스, 이미지경로, 비트맵, 원본이미지크기, 플래그
+	m_BitMap = (HBITMAP)LoadImageA(NULL, FileName, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION | LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	//붓에 잉크를 찍는.
+	SelectObject(MemDC, m_BitMap);
+	BITMAP BitMap_Info;
+	GetObject(m_BitMap, sizeof(BitMap_Info), &BitMap_Info);
+	m_Size.cx = BitMap_Info.bmWidth;
+	m_Size.cy = BitMap_Info.bmHeight;
+}
+void BitMap::Draw(HDC hdc, int x, int y)
+{
+	//크기 변경이 가능한 이미지.
+	//화면상 좌표, 화면상에 그려지는 너비 높이, 원본 이미지 xy좌표, 
+	StretchBlt(hdc, x, y, m_Size.cx, m_Size.cy, MemDC, 0, 0, m_Size.cx, m_Size.cy, SRCCOPY);
+}
+
+BitMap::~BitMap()
+{
+	DeleteObject(m_BitMap);
+	DeleteDC(MemDC);
+}
---
+++ WinAPI/Card.cpp
@@ -0,0 +1,47 @@
+#include "Card.h"
+
+
+
+Card::Card()
+{
+	//카드는 후방 상태로(뒤집혀 있는 상태)
+	m_eCardState = CARD_REAR;
+}
+
+
+void Card::Init(IMAGE Index, int x, int y)
+{
+	m_pBitMap[CARD_FRONT] = BitMapManager::GetInstance()->GetImage(Index);
+	m_pBitMap[CARD_REAR] = BitMapManager::GetInstance()->GetImage(IMAGE_BLACK);
+	m_ix = x; //500
+	m_iy = y; //200
+	m_BitMapRect.left = x; 
+	m_BitMapRect.top = y; 
+	m_BitMapRect.right = m_BitMapRect.left + m_pBitMap[CARD_FRONT]->GetSize().cx;
+	m_BitMapRect.bottom = m_BitMapRect.top + m_pBitMap[CARD_FRONT]->GetSize().cy;
+}
+void Card::Draw(HDC hdc)
+{
+	//카드 상태에 따라 비트맵의 StretchBlt 작업 수행
+	m_pBitMap[m_eCardState]->Draw(hdc, m_ix, m_iy);
+}
+//
+bool Card::ColliderCheck(POINT point)
+{
+	//체크할 사각형의 영역과 x,y(대상의 위치)
+	if (PtInRect(&m_BitMapRect, point))
+	{
+		//충돌한 상태에서 카드가 앞면일때 뒷면으로
+		//반대인 상황에선 앞면으로
+		if (m_eCardState == CARD_FRONT)
+			m_eCardState = CARD_REAR;
+		else
+			m_eCardState = CARD_FRONT;
+		return true;
+	}
+	return false;
+}
+
+Card::~Card()
+{
+}
